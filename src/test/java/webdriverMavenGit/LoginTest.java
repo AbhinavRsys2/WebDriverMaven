bfb9ac65d88d3773caeafab11daa392a5e8a051a
Abhinav <Abhinav@C40LAP-10041.india.rsystems.com>
2021-03-12 17:10:31 +0530

    Initial Commit

---
+++ src/test/java/webdriverMavenGit/LoginTest.java
@@ -0,0 +1,25 @@
+package webdriverMavenGit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	public static WebDriver driver;
+	
+	@BeforeTest
+	public void setup(){
+		
+		driver = new ChromeDriver();
+		
+	}
+	
+	@Test
+	public void login()
+	{
+		driver.get("https://gmail.com");
+		System.out.println("The title of page is -> "+driver.getTitle());
+	}
+}
